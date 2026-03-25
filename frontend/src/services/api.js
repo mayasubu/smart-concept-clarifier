a74dbb0d6307a1dcad6069f99daf29cb287ebf1d
mayasubu <subumaya512@gmail.com>
2026-03-25 10:44:28 +0530

    connected frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://smart-concept-clarifier.onrender.com/api';
 
 const api = axios.create({ baseURL: API_BASE });
 
